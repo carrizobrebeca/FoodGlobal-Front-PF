bc58671a48b1cca2367072b83c6a8f32b96d41a0
santiperez98 <santip19982022@gmail.com>
2024-08-22 00:19:40 -0300

    Initial commit with React setup

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <h1 className="text-4xl font-bold text-blue-600">
-        ¡Bienvenido a FoodGlobal!
+        ¡Bienvenido a FoodGlobal .! 
       </h1>
     </div>
   );
